1a7f8218fc583b164fef0583d2762943bdf1582e
Andrew Yu <andrew.yu6@hotmail.com>
2017-04-16 23:53:48 +1000

    Add files via upload

--- pixelColor.c
+++ pixelColor.c
@@ -12,9 +12,6 @@
 #define MAX_INTENSITY 255 
 //765 
 unsigned char stepsToRed (int steps){
-   return steps; 
-}
-unsigned char stepsToBlue (int steps){
    unsigned char returnValue = steps*3 - MAX_INTENSITY;
    if(returnValue < 0){
       returnValue = 0;
@@ -23,12 +20,15 @@ unsigned char stepsToBlue (int steps){
    }
    return returnValue; 
 }
-unsigned char stepsToGreen (int steps){
-      unsigned char returnValue = steps*3 - (MAX_INTENSITY * 2);
+unsigned char stepsToBlue (int steps){
+   unsigned char returnValue = steps*3 - (MAX_INTENSITY * 2);
    if(returnValue < 0){
       returnValue = 0;
    } else if(returnValue > MAX_INTENSITY*2){
       returnValue = MAX_INTENSITY; 
    }
    return returnValue; 
+}
+unsigned char stepsToGreen (int steps){
+   return steps;
 }

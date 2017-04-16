d0456a121fca50246b8199b2119f5bce301e6def
Andrew Yu <andrew.yu6@hotmail.com>
2017-04-17 00:11:49 +1000

    Add files via upload

---
+++ xanadu.h
@@ -0,0 +1,13 @@
+/*
+ * xanadu.h
+ * defines the centre of your beautiful colored 
+ * tile image of the mandelbrot set
+ * Andrew Yu & Alex Huang
+ * A creepy pattern that reminds us of arachnids 
+ * 17/04/17 
+ */
+ #define CENTER_X -1.25
+ #define CENTER_Y -0.0605 
+ #define ZOOM 20 
+ 
+//http://localhost:1917/tile_x-1.2500_y-0.0605_z20.bmp
